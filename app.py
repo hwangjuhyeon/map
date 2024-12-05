3c4d9ea17e5367b5c09288ead7b1deeb426d9ea7
hwangjuhyeon <juhyeon5318@gmail.com>
2024-12-05 18:03:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,38 +5,12 @@ st.title("동선 프로그램")
 # Kakao 지도 API 키
 KAKAO_API_KEY = "dd5a7e1db353ceee0ef645b2cb146dac"
 
-def render_kakao_map():
-    kakao_map_html = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Kakao Map</title>
-        <script src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={KAKAO_API_KEY}"></script>
-        <style>
-            #map {{
-                width: 100%;
-                height: 500px;
-            }}
-        </style>
-    </head>
-    <body>
-        <div id="map"></div>
-        <script>
-            // Kakao 지도 초기화
-            var mapContainer = document.getElementById('map'); // 지도를 표시할 div
-            var mapOption = {{
-                center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울 중심 좌표
-                level: 3 // 확대 레벨
-            }};
-            var map = new kakao.maps.Map(mapContainer, mapOption); // 지도 생성
-        </script>
-    </body>
-    </html>
-    """
-    return kakao_map_html
+import requests
 
-
-# Streamlit 앱
-st.title("Kakao Map Integration")
-st.components.v1.html(render_kakao_map(), height=500)
+searching = '합정 스타벅스'
+url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(searching)
+headers = {
+    "Authorization": "KakaoAK dd5a7e1db353ceee0ef645b2cb146dac"
+}
+places = requests.get(url, headers = headers).json()['documents']
+places
