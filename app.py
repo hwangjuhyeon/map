ec55a4f18b96e365ab81215e00b5b9c5c14a113d
hwangjuhyeon <juhyeon5318@gmail.com>
2024-12-05 18:08:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,27 @@ import streamlit as st
 
 st.title("동선 프로그램")
 
-# Kakao 지도 API 키
-KAKAO_API_KEY = "dd5a7e1db353ceee0ef645b2cb146dac"
+# Kakao 지도 HTML 삽입
+def kakao_map(api_key):
+    st.write("""
+    <script type="text/javascript"
+            src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={}"></script>
+    <div id="map" style="width:100%;height:400px;"></div>
+    <script>
+        var container = document.getElementById('map');
+        var options = {{
+            center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울 시청 좌표
+            level: 3 // 확대 레벨
+        }};
+        var map = new kakao.maps.Map(container, options);
+    </script>
+    """.format(api_key), unsafe_allow_html=True)
 
+def main():
+    st.title("Kakao 지도 API 예제")
+    kakao_api_key = st.text_input("Kakao API 키를 입력하세요", "")
+    if kakao_api_key:
+        kakao_map(kakao_api_key)
+
+if __name__ == "__main__":
+    main()
