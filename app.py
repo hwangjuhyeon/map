b5b3a7189d1f67a88ee7740e0b301863874c0190
hwangjuhyeon <juhyeon5318@gmail.com>
2024-12-05 18:05:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,3 @@ st.title("동선 프로그램")
 # Kakao 지도 API 키
 KAKAO_API_KEY = "dd5a7e1db353ceee0ef645b2cb146dac"
 
-import requests
-
-searching = '합정 스타벅스'
-url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(searching)
-headers = {
-    "Authorization": "KakaoAK dd5a7e1db353ceee0ef645b2cb146dac"
-}
-places = requests.get(url, headers = headers).json()['documents']
-places
