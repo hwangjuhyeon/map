fafe1c56eddc2b80e8ea9e250ad05ea127ea01ca
hwangjuhyeon <juhyeon5318@gmail.com>
2024-12-06 22:53:17 +0900

    Update map.py

--- map.py
+++ map.py
@@ -9,9 +9,7 @@ response = client.chat.completions.create(
  messages=[
  {"role": "system", "content": "You are a helpful assistant."},
  {"role": "user", "content": "Who won the world series in 2020?"},
- {"role": "assistant", "content": "The Los Angeles Dodgers won the World
-Series in 2020."},
  {"role": "user", "content": "Where was it played?"}
- ] # 하이퍼파라미터, functions 등 생략된 arguments 있음
+ ] 
 )
 print(response.choices[0].message.content)
